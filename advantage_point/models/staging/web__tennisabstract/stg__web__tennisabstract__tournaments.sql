37c97f259b70c767f232702d9c27fb3a2aa27ef1
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-09-11 21:40:39 -0400

    Add stg__web__tennisabstract__tournaments

---
+++ advantage_point/models/staging/web__tennisabstract/stg__web__tennisabstract__tournaments.sql
@@ -0,0 +1,25 @@
+with
+
+source as (
+    select
+        *
+    from {{ source('web__tennisabstract', 'tournaments') }}
+),
+
+renamed as (
+    select
+        tournament_url,
+        cast(tournament_year as integer) as tournament_year,
+        tournament_gender,
+        tournament_name,
+        tournament_title,
+        parse_date('%B %d, %Y', tournament_start_date) as tournament_start_date,
+        tournament_surface,
+        cast(tournament_draw_size as int) as tournament_draw_size,
+        audit_column__active_flag,
+        audit_column__start_datetime_utc,
+        audit_column__end_datetime_utc
+    from source
+)
+
+select * from renamed
