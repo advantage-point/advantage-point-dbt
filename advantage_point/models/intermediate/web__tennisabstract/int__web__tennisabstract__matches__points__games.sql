21b334544f47f7d0233ab8c0742b79186dbf6d12
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-09-18 09:33:28 -0400

    Remove extra description from bk columns

---
+++ advantage_point/models/intermediate/web__tennisabstract/int__web__tennisabstract__matches__points__games.sql
@@ -0,0 +1,54 @@
+with
+
+tennisabstract_matches_points as (
+    select * from {{ ref('int__web__tennisabstract__matches__points') }}
+),
+
+-- get game columns
+tennisabstract_matches_points_games as (
+    select
+        *
+    from tennisabstract_matches_points
+    where point_score_in_game = '0-0' -- filter for 'beginning of game' rows
+),
+
+-- create tiebreaker flag
+tennisabstract_matches_points_games_tiebreaker as (
+    select
+        *,
+
+        case
+            when game_score_in_set in ('6-6') then true
+            when game_score_in_set not in ('6-6') then false
+            else null
+        end as is_tiebreaker,
+    
+    from tennisabstract_matches_points_games
+),
+
+final as (
+    select
+        bk_game,
+        bk_match,
+        game_number_in_match,
+
+        match_url,
+
+        bk_set,
+        game_number_in_set,
+
+        point_server as game_server,
+        point_receiver as game_receiver,
+        bk_point_server as bk_game_server,
+        bk_point_receiver as bk_game_receiver,
+
+        game_score_in_set,
+        game_score_in_set_server,
+        game_score_in_set_receiver,
+
+        is_tiebreaker,
+
+    from tennisabstract_matches_points_games_tiebreaker
+)
+
+select * from final
