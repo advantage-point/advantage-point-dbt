f4c3efda7a52082a43f943d7eab6f7179a258027
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-11-04 10:58:43 -0500

    Trim tournament models

--- advantage_point/models/intermediate/int__tournaments.sql
+++ advantage_point/models/intermediate/int__tournaments.sql
@@ -1,11 +1,25 @@
 with
 
 tennisabstract_tournaments as (
-    select * from {{ ref('int__web__tennisabstract__tournaments') }}
+    select
+        *,
+        {{ generate_bk_tournament(
+            tournament_year_col='tournament_year',
+            tournament_event_col='tournament_event',
+            tournament_name_col='tournament_name'
+        )}} as bk_tournament,
+    from {{ ref('stg__web__tennisabstract__tournaments') }}
+    where audit_column__active_flag = true
 ),
 
-tennisabstract_matches_tournaments as (
-    select * from {{ ref('int__web__tennisabstract__matches__tournaments') }}
+tennisabstract_matches as (
+    select
+        *,
+        extract(year from match_date) as tournament_year,
+        match_event as tournament_event,
+        match_tournament as tournament_name,
+    from {{ ref('stg__web__tennisabstract__matches') }}
+    where audit_column__active_flag = true
 ),
 
 tennisabstract_tournament_formats as (
@@ -16,6 +30,35 @@ tennisabstract_tournament_formats as (
 tennisabstract_tournament_formats_default as (
     select * from tennisabstract_tournament_formats
     where lower(tournament_name) = '_default'
+),
+
+-- get tournaments from match data
+tennisabstract_match_tournaments as (
+    select distinct
+        {{ generate_bk_tournament(
+            tournament_year_col='tournament_year',
+            tournament_event_col='tournament_event',
+            tournament_name_col='tournament_name'
+        )}} as bk_tournament,
+        tournament_year,
+        tournament_event,
+        tournament_name,
+
+        -- order by tournament DESC so that the 'more capitalized' version captured
+        -- in the example where there are 2 rows:
+            -- Rio de Janeiro
+            -- Rio De Janeiro
+        row_number() over (partition by tournament_year, tournament_event, lower(tournament_name) order by tournament_name desc) as tournament_row_num
+
+    from tennisabstract_matches
+),
+
+-- filter for row number
+tennisabstract_match_tournaments_row_num as (
+    select
+        *
+    from tennisabstract_match_tournaments
+    where tournament_row_num = 1
 ),
 
 -- union tournaments
@@ -38,7 +81,7 @@ tournaments_union as (
                 tournament_year,
                 tournament_event,
                 tournament_name
-            from tennisabstract_matches_tournaments
+            from tennisabstract_match_tournaments_row_num
         )
     ) as t
 ),
@@ -49,7 +92,9 @@ final as (
         t.tournament_year,
         t.tournament_event,
         t.tournament_name,
-        t_ta.bk_tournament_start_date,
+        {{ generate_bk_date(
+            date_col='t_ta.tournament_start_date'
+        ) }} as bk_tournament_start_date,
         t_ta.tournament_start_date,
         t_ta.tournament_surface,
         t_ta.tournament_draw_size,
@@ -105,7 +150,7 @@ final as (
 
     from tournaments_union as t
     left join tennisabstract_tournaments as t_ta on t.bk_tournament = t_ta.bk_tournament
-    left join tennisabstract_matches_tournaments as m_ta on t.bk_tournament = m_ta.bk_tournament
+    left join tennisabstract_match_tournaments_row_num as m_ta on t.bk_tournament = m_ta.bk_tournament
     left join tennisabstract_tournament_formats as tf on t.bk_tournament = tf.bk_tournament
     left join tennisabstract_tournament_formats_default as tf_default on lower(t.tournament_event) = lower(tf_default.tournament_event)
 )
