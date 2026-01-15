28055f8027fb9813d0e07c08e9665cb21820c196
Aneesh Kodali <aneesh.kodali@gmail.com>
2026-01-15 02:59:18 +0000

    Update generate_schema_name

--- advantage_point/macros/generate_schema_name.sql
+++ advantage_point/macros/generate_schema_name.sql
@@ -1,9 +1,5 @@
+-- put this in macros/generate_schema_name.sql
+
 {% macro generate_schema_name(custom_schema_name, node) -%}
-
-    {%- if custom_schema_name is not none -%}
-        {{ custom_schema_name | trim }}
-    {%- else -%}
-        {{ target.schema }}
-    {%- endif -%}
-
+    {{ generate_schema_name_for_env(custom_schema_name, node) }}
 {%- endmacro %}
