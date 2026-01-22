8d6826d08e439972de3e67e07b797bc2c79953be
Aneesh Kodali <aneesh.kodali@gmail.com>
2026-01-22 00:49:45 -0500

    Redo fact_match_player model

---
+++ advantage_point/models/warehouse/fact/fact_match_player.sql
@@ -0,0 +1,49 @@
+with
+
+match_players as (
+    select * from {{ ref('int_match_player') }}
+),
+
+-- generate surrogate key
+match_player_sks as (
+    select
+        *,
+
+        {{ generate_sk_match_player(
+                bk_match_col='bk_match',
+                bk_player_col='bk_player'
+        ) }} as sk_match_player,
+
+        {{ generate_sk_match(
+            bk_match_col='bk_match'
+        ) }} as sk_match,
+
+        {{ generate_sk_player(
+            bk_player_col='bk_player'
+        ) }} as sk_player,
+    
+    from match_players
+),
+
+final as (
+    select
+
+        -- surrogate key
+        mp.sk_match_player,
+
+        -- foreign keys
+        mp.sk_match,
+        mp.sk_player,
+
+        -- business keys
+        mp.bk_match,
+        mp.bk_player,
+
+        -- attributes
+        mp.player_is_winner,
+        mp.player_score,
+
+    from match_player_sks as mp
+)
+
+select * from final
