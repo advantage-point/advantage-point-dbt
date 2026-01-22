52ac8918c813bcaa7fba132a848258dc90431c50
Aneesh Kodali <aneesh.kodali@gmail.com>
2026-01-21 23:28:45 -0500

    Redo int_point model

--- advantage_point/models/intermediate/int_point.sql
+++ advantage_point/models/intermediate/int_point.sql
@@ -4,14 +4,6 @@ tennisabstract_points as (
     select * from {{ ref('int_tennisabstract__points_enriched') }}
 ),
 
-matches as (
-    select * from {{ ref('int_match') }}
-),
-
-tournaments as (
-    select * from {{ ref('int_tournament') }}
-),
-
 -- union points
 points_union as (
     select distinct
@@ -33,6 +25,7 @@ final as (
         p.bk_match,
         p.point_number_in_match,
 
+        p_ta.bk_set,
         p_ta.bk_game,
 
         p_ta.point_result,
@@ -53,12 +46,13 @@ final as (
         -- get side of court
         case
             -- determine side based on non-tiebreaker scores
+            -- TODO: figure out if ad scoring is used
             when p_ta.point_score_in_game in (
                 '0-0', '0-30',
                 '15-15', '15-40',
                 '30-0', '30-30'
             ) then 'deuce'
-            when p_ta.point_score_in_game = '40-40' and t.is_ad_scoring then 'deuce'
+            when p_ta.point_score_in_game = '40-40' then 'deuce'
             when p_ta.point_score_in_game in (
                 '0-15', '0-40',
                 '15-0', '15-30',
@@ -73,8 +67,6 @@ final as (
 
     from points_union as p
     left join tennisabstract_points as p_ta on p.bk_point = p_ta.bk_point
-    left join matches as m on p.bk_match = m.bk_match
-    left join tournaments as t on m.bk_tournament = t.bk_tournament
 )
 
 select * from final
